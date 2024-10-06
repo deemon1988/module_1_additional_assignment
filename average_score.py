73a544440573fb07f89d5dbfef44f77bd33b2180
deemon1988 <dmn72835@gmail.com>
2024-10-06 15:26:14 +0300

    Готовый вариант решения. Задание "Средний балл":

--- average_score.py
+++ average_score.py
@@ -1,28 +1,26 @@
 #  Дополнительное практическое задание по модулю: "Базовые структуры данных."
-from typing import final
 
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
 
-list_students = list(students)
-list_students.sort()
-print(list_students)
 
-def complete_dict(*, grades: list, students: list) -> dict[str, float]:
+def complete_dict(*, grades: list, students: set) -> dict[str, float]:
+    list_students = list(students)
+    list_students.sort()
     list_avg_grades = []
     for student_grade in grades:
         avg_grade = sum(student_grade) / len(student_grade)
         list_avg_grades.append(round(avg_grade, 1))
 
-    dict_students = {}
-    for i in range(len(students)):
-        dict_students[list_students[i]] = list_avg_grades[i]
+    dict_ = {}
+    for i in range(len(list_students)):
+        dict_[list_students[i]] = list_avg_grades[i]
 
-    return dict_students
+    return dict_
 
+dict_students = complete_dict(grades=grades, students=students)
+print('Словарь средних оценок студентов:', dict_students)
 
-print('Словарь средних оценок студентов:', complete_dict(grades=grades, students=list_students))
-print(type(complete_dict(grades=grades, students=list_students)))
 
 
 def nothing():
